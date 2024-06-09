5382f3bb6a6a47205340c972ebe523baddd23c73
seonkyung <yisjoo0617@gmail.com>
2024-06-10 00:16:53 +0900

    디렉토리 구조 변경

--- learn-typescript-master/quiz/1_todo/src/index.ts
+++ learn-typescript-master/quiz/1_todo/src/index.ts
@@ -7,7 +7,7 @@ interface TodoItem {
 let todoItems: TodoItem[];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): TodoItem[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -17,40 +17,42 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
-  const todos: TodoItem[] = fetchTodoItems();
+function fetchTodos(): TodoItem[] {
+  const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo: TodoItem) {
+function addTodo(todo: TodoItem): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index: number) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: TodoItem) {
+function completeTodo(index: number, todo: TodoItem): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): TodoItem {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): TodoItem[] {
   return todoItems.filter((item: TodoItem) => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  this.addTodo();
+  this.addTodo();
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
